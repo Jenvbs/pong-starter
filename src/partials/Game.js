675ef6639a6cedc58b343666819df17cccf9d718
Danny Smith <hi@danny.is>
2019-05-02 12:57:12 +0100

    Reformat code

--- src/partials/Game.js
+++ src/partials/Game.js
@@ -1,13 +1,20 @@
-export default class Game {
-  constructor(element, width, height) {
-    this.element = element;
-    this.width = width;
-    this.height = height;
+import { SVG_NS } from '../settings'
 
-		// Other code goes here...
+export default class Game {
+  constructor(elementID, width, height) {
+    this.elementID = elementID
+    this.width = width
+    this.height = height
+
+    this.gameElement = document.getElementById(elementID)
   }
 
   render() {
-		// More code goes here....
+    // Create SVG
+    const svg = document.createElementNS(SVG_NS, 'svg')
+    svg.setAttributeNS(null, 'width', this.width)
+    svg.setAttributeNS(null, 'height', this.height)
+    svg.setAttributeNS(null, 'viewBox', `0 0 ${this.width} ${this.height}`)
+    this.gameElement.appendChild(svg)
   }
 }
---
+++ src/settings.js
@@ -0,0 +1,2 @@
+// This stores constants which should hold settings data.
+export const SVG_NS = "http://www.w3.org/2000/svg";
