28e6c9fba163c8379b83684c44683798838a2f5f
Jen <vobis.jennifer@yahoo.co.uk>
2019-05-06 11:09:07 +0100

    displayed the score on each side

--- src/partials/Scoreboard.js
+++ src/partials/Scoreboard.js
@@ -6,19 +6,32 @@ export default class Scoreboard {
     this.x = x;
     this.y = y;
     this.size = size;
+    this.score = 0;
   }
 
     render(svg) {
       const text = document.createElementNS(SVG_NS, 'text')
-      text.setAttributeNS(null, 'x', this.x)
-      text.setAttributeNS(null, 'y', this.y)
+      text.setAttributeNS(null, 'x', this.x /3.5)
+      text.setAttributeNS(null, 'y', this.y / 2)
       text.setAttributeNS(null, 'font-family', '"Silkscreen Web", monotype')
       text.setAttributeNS(null, 'font-size', this.size)
       text.setAttributeNS(null, 'fill', '#665')
       svg.appendChild(text)
 
-      text.textContent = score
+      text.textContent = this.score
 
       svg.appendChild(text)
+
+      const textTwo = document.createElementNS(SVG_NS, 'text')
+      textTwo.setAttributeNS(null, 'x', this.x / 1.45)
+      textTwo.setAttributeNS(null, 'y', this.y / 2)
+      textTwo.setAttributeNS(null, 'font-family', '"Silkscreen Web", monotype')
+      textTwo.setAttributeNS(null, 'font-size', this.size)
+      textTwo.setAttributeNS(null, 'fill', '#665')
+      svg.appendChild(textTwo)
+
+      textTwo.textContent = this.score
+
+      svg.appendChild(textTwo)
     }
   }
