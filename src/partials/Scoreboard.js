4afb0c08c22e0876fdbd136937c7e4f8533cc952
Jen <vobis.jennifer@yahoo.co.uk>
2019-05-06 10:04:12 +0100

    added stuff to the scoreboard

--- src/partials/Scoreboard.js
+++ src/partials/Scoreboard.js
@@ -2,27 +2,23 @@ import { SVG_NS } from '../settings'
 import helpers from '../helpers'
 
 export default class Scoreboard {
-    constructor(width, height) {
-      this.width = width / 2
-      this.height = height / 2
-    }
+  constructor(x, y, size) {
+    this.x = x;
+    this.y = y;
+    this.size = size;
+  }
 
     render(svg) {
-      const rect = document.createElementNS(SVG_NS, 'rect')
-      rect.setAttributeNS(null, 'fill', '#353535')
-      rect.setAttributeNS(null, 'width', this.width)
-      rect.setAttributeNS(null, 'height', this.height)
+      const text = document.createElementNS(SVG_NS, 'text')
+      text.setAttributeNS(null, 'x', this.x)
+      text.setAttributeNS(null, 'y', this.y)
+      text.setAttributeNS(null, 'font-family', '"Silkscreen Web", monotype')
+      text.setAttributeNS(null, 'font-size', this.size)
+      text.setAttributeNS(null, 'fill', '#665')
+      svg.appendChild(text)
 
-      const line = document.createElementNS(SVG_NS, 'line')
-      line.setAttributeNS(null, 'x1', this.width / 2)
-      line.setAttributeNS(null, 'y1', 0)
-      line.setAttributeNS(null, 'x2', this.width / 2)
-      line.setAttributeNS(null, 'y2', this.height)
-      line.setAttributeNS(null, 'stroke', 'white')
-      line.setAttributeNS(null, 'stroke-dasharray', '20, 15')
-      line.setAttributeNS(null, 'stroke-width', '4')
+      text.textContent = score
 
-      svg.appendChild(rect)
-      //svg.appendChild(line)
+      svg.appendChild(text)
     }
   }
